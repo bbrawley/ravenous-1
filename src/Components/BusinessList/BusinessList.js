bc6cc1ba2e4954f8c56e42ac0ed5e9af191c9a2c
bbrawley <brandon@iempireapps.com>
2017-12-06 17:52:48 -0500

    Add files via upload

--- src/Components/BusinessList/BusinessList.js
+++ src/Components/BusinessList/BusinessList.js
@@ -5,6 +5,7 @@ import Business from  '../Business/Business';
 
 class BusinessList extends React.Component{
     render(){
+                console.log(this);
         return (
         <div className="BusinessList">
             {
