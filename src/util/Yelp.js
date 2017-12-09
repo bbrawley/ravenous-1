0461060a45bb52c1eefec76e6d0f412da86bce85
bbrawley <brandon@iempireapps.com>
2017-12-09 15:28:38 -0500

    Add files via upload

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -30,13 +30,13 @@ const Yelp = {
                             id: business.id,
                             imageSrc: business.image_url,
                             name: business.name,
-                            address: business.location,
-                            city: business.city,
-                            state: business.state,
-                            zipCode: business.zipCode,
+                            address: business.location.address1,
+                            city: business.location.city,
+                            state: business.location.state,
+                            zipCode: business.location.zip_code,
                             category: business.categories[0].title,
                             rating: business.rating,
-                            reviewCount: business.review_Count,
+                            reviewCount: business.review_count,
                         }
                     )
                   )
